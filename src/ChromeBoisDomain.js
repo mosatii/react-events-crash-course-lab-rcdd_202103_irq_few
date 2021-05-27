c5dc2658b471c0afd6ed0f309010f74539778950
Mousa  Rajab <aljawaherimusa@gmail.com>
2021-05-27 08:47:19 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -4,11 +4,11 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 
 export default class ChromeBoisDomain extends Component {
-  
+
   handleMouseMove = (event) => {
     drawChromeBoiAtCoords(event.clientX, event.clientY)
   }
-  
+
   handleKeyDown = (event) => {
     if (event.key === 'a') {
       resize('+')
@@ -16,10 +16,10 @@ export default class ChromeBoisDomain extends Component {
       resize('-')
     }
   }
-   
+
   render() {
     return (
-      <canvas 
+      <canvas
         onMouseMove={this.handleMouseMove}
         onClick={() => {toggleCycling()}}
         onKeyDown={this.handleKeyDown}
